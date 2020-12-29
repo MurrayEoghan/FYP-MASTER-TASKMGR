54cf36e80594a11e5a0fdb3238e2e230b81d4740
Eoghan Murray <eoghan.murray@mycit.ie>
2020-12-29 19:36:46 +0000

    specified router path types

--- src/settings/router.go
+++ src/settings/router.go
@@ -13,8 +13,8 @@ func Router() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	v1Router := router.PathPrefix("/api/v1").Subrouter()
-	v1Router.HandleFunc("/login", controller.UserLogIn)
-	v1Router.HandleFunc("/signup", controller.UserSignUp)
+	v1Router.HandleFunc("/login", controller.UserLogIn).Methods(http.MethodGet)
+	v1Router.HandleFunc("/signup", controller.UserSignUp).Methods(http.MethodPost)
 	// v1Router.HandleFunc("/login", repo.GetUserByUsernameAndPassword).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
